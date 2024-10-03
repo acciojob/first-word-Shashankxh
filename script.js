c7e3a05b4123a682a0873388f59f158c85085224
Shashank <139771571+Shashankxh@users.noreply.github.com>
2024-10-03 10:35:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function firstWord(s) {
-  // your code here
+  // Trim the string to remove leading and trailing whitespaces
+  s = s.trim();
+  
+  // Find the index of the first space
+  const firstSpaceIndex = s.indexOf(' ');
+
+  // If there's no space, return the whole string
+  if (firstSpaceIndex === -1) {
+    return s;
+  }
+
+  // Otherwise, return the substring from the beginning to the first space
+  return s.substring(0, firstSpaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
